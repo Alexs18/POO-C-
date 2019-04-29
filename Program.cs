0a97c037d5edeebcb84110f62b3ba210af3f0507
Alexs18 <50090511+Alexs18@users.noreply.github.com>
2019-04-28 22:08:04 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,33 @@
 ﻿using System;
 
-namespace ConsoleApp13
+namespace ConsoleApp12
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int boton;
-            do
+            Console.WriteLine("Ejercicio 4 crear un programa para calcular el salario mensual de unos empleados a los que" +
+                "se les paga 15 dolares por hora, si estas no superan las 35, cada hora por encima de 35 se considera extra " +
+                "y se paga a 22 dolares, el programa pide las horas del trabajador, y devuelve el salario que se debe pagar");
+            Console.WriteLine("DIGITE SUS HORAS TRABAJADAS"); int horas = Convert.ToInt32(Console.ReadLine());
+            double total=0,horast=0,extra=0, total1=0,total2;
+            if (horas<=35)
             {
-                Console.WriteLine("  ");
-            Console.WriteLine("    BIENVENIDO \n" +
-                       "AL MENU DE OPCCIONES\n" +
-                "\n 1.- Salir"  +
-                "\n 2- Sumatorio"  +
-                "\n 3.-Factorial\n");
-
-                Console.WriteLine("DIGITE SU OPCCION\n");
-                boton = Convert.ToInt32(Console.ReadLine());
-
-            switch (boton)
-            {
-                case 1:
-                    Console.WriteLine("USTED A ELEJIDO LA OPCCION SALIR\n  " +
-                        "DIGITE CUALQUIER TECLA PARA SALIR");
-                    
-                    break;
-
-                case 2:
-                        Console.WriteLine("USTED A ELEJIDO LA OPCCION SUMATORIO\n");
-                        Console.WriteLine("Elija el numero a calcular el sumatorio\n"); int n1 = Convert.ToInt32(Console.ReadLine());
-                        int T = 0;
-                         for (int i =n1; i!=0; i--)
-                         {   T = T + i;  }
-                         Console.WriteLine("el sumatorio es\n "+T);
-                    break;
-                case 3:
-                    Console.WriteLine("USTED A ELEIDO LA OPCCION FATORIAL\n");
-                        Console.WriteLine("Digite el numero a calcular el factorial\n"); int n = Convert.ToInt32(Console.ReadLine());
-                        int T1=1;
-                        for (int i=1;i<=n;i++)
-                        {
-                            T1 = T1 * i;
-                        }
-                        Console.WriteLine("el factorial es "+ T1);
-                    
-                    break;
-                    
-                default:
-                    Console.WriteLine("no se a seleccionado una opccion");
-                    break;
+                total = horas * 15;
+                Console.WriteLine("SU TOTAL A PAGAR ES DE :" + total);
             }
-            } while (boton!=1);
+            else
+            {
+                horast = horas - 35;
+                extra = horast * 22;
+                total1 = horas - horast;
+                total2 = total1 * 15;
+                total = extra + total2;
+                Console.WriteLine("SU TOTAL A PAGAR ES:" + total);
+                    
+            }
             Console.ReadKey();
 
-           
         }
     }
 }
