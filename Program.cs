c6b89e8e73f5ebbc27711f63c1e8e877f44ca286
Alexs18 <50090511+Alexs18@users.noreply.github.com>
2019-04-28 22:12:46 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,43 @@
 ﻿using System;
 
-namespace ConsoleApp8
+namespace ConsoleApp7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //CREAR UN PROGRAMA QUE INGRESE UN NUMERO, Y A PARTIR DE ESE NUMERO CREE UN CUADRADO, SOLAMENTE LOS BORDES.
-            Console.WriteLine("Ingrese cantidad del cuadrado");int c = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i <c; i++)
+            //crear un programa que lea una factura
+            //1 lea la cantidad //2 introduce precio //termina lectura cantidad sea cero y da el total
+            double t=0;
+            Console.WriteLine("DIGITE CANTIDAD DE PRODUCTOS");int p = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < p; i++)
             {
-                for (int a= 0; a < c; a++) 
-                {
-                    if (i==0||a==0|| a == c - 1|| i == c - 1) 
-                    {
-                        Console.Write("*");   } 
-                    else
-                    {
-                        Console.Write(" "); 
-                    }
+
+            Console.WriteLine("DIGITE NUMERO DE VENTAS"); int V = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("DIGITE EL PRECIO"); double pr = Convert.ToInt32(Console.ReadLine());
+
+            if (V==0)
+            {
+                Console.WriteLine("EJECUCION FINALIZADA");
+
+            }
+            else
+            {
+                t = V * pr;
+                Console.WriteLine("CANTIDAD DE PRODUCTOR ADQUIRIDOS ES  "+ V);
+                Console.WriteLine("PRECIO POR CANTIDAD  "+ pr);
+                    Console.WriteLine("TOTAL A PAGAR " + t);
+
                 }
-                Console.WriteLine("");
             }
             Console.ReadKey();
+
+
+
+
         }
+
     }
+
 }
