200daed4216bef79ed35e0fbcb3bd3dedd02f134
Alexs18 <50090511+Alexs18@users.noreply.github.com>
2019-04-28 22:09:26 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,28 @@
 ﻿using System;
 
-namespace ConsoleApp12
+namespace ConsoleApp14
 {
     class Program
     {
         static void Main(string[] args)
+            //CREE UN PROGRAMA QUE RECIBA COMO PARAMETRO UN NUMERO ENTERO
+            // Y A PARTIR DE EL CREE UNA PIRAMIDE
         {
-            Console.WriteLine("Ejercicio 4 crear un programa para calcular el salario mensual de unos empleados a los que" +
-                "se les paga 15 dolares por hora, si estas no superan las 35, cada hora por encima de 35 se considera extra " +
-                "y se paga a 22 dolares, el programa pide las horas del trabajador, y devuelve el salario que se debe pagar");
-            Console.WriteLine("DIGITE SUS HORAS TRABAJADAS"); int horas = Convert.ToInt32(Console.ReadLine());
-            double total=0,horast=0,extra=0, total1=0,total2;
-            if (horas<=35)
+            Console.WriteLine("Ingrese cantidad");
+            int lexs = Convert.ToInt16(Console.ReadLine());
+            int i, j, a;
+           
+            for (i = 0; i < lexs; i++)
             {
-                total = horas * 15;
-                Console.WriteLine("SU TOTAL A PAGAR ES DE :" + total);
-            }
-            else
-            {
-                horast = horas - 35;
-                extra = horast * 22;
-                total1 = horas - horast;
-                total2 = total1 * 15;
-                total = extra + total2;
-                Console.WriteLine("SU TOTAL A PAGAR ES:" + total);
-                    
+                for (j = i; j < lexs; j++)
+                { Console.Write(" "); }
+                for (a = 0; a != i; a++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine("*");
             }
             Console.ReadKey();
-
         }
     }
 }
